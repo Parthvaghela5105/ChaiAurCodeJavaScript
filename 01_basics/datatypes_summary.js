b61359f632dfa9e77aff3b615ded9be078c1bfbe
Parthvaghela5105 <133079004+Parthvaghela5105@users.noreply.github.com>
2024-05-17 15:22:58 +0000

    datatypes summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,22 @@
+/*
+    primitive DataTypes
+    
+    7 Types:String , Number , Boolean , null , undefiened , symbol , BigInt
+*/
+
+
+
+/* 
+    Reference(Non primitive)
+    Array , Objects , Functions 
+*/
+
+const heros = ["Shaktiman" , "naagraj" , "doga"]
+let myObj = {
+    name : "Parth",
+    age : 19,
+}
+
+const myFunction = function() {
+    console.log("My name is Parth");
+}
