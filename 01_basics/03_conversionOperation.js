fba6e1bfdc9ae8db041de2dcddf6bcb9faf642f1
Parthvaghela5105 <133079004+Parthvaghela5105@users.noreply.github.com>
2024-05-17 14:48:18 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+/*
+    "33" => 33
+    "33abc" => NaN
+    null => 0
+    true => 1
+    false => 0
+    undefiened => NaN
+*/
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+/*
+    1 => true , 0 => false
+    ""=> false
+    "Parth => true"
+*/
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
